326e7371b193bcb95d170007fb4a7682c93257d0
zhongjunji <zhongjunji@jd.com>
2023-01-30 14:19:21 +0800

    feat: 添加react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 const div = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(div)
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+  {
+    path: '/test',
+    element: <div>123</div>
+  }
+])
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes}></RouterProvider>
   </React.StrictMode>
 )
