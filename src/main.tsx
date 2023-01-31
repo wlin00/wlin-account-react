14d286c843cbff488f7a039b83fe1326046f41e5
wlin00 <616294069@qq.com>
2023-01-31 14:24:45 +0800

    feat: 目录结构修改

--- src/main.tsx
+++ src/main.tsx
@@ -1,58 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { RedirectPage } from './components/RedirectPage'
-import { SpringDemo } from './components/SpringDemo';
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './routes/router';
 
 const div = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(div)
-const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <Outlet/>, // Outlet 类似vue中的 <RouterView />
-    errorElement: <RedirectPage/>,
-    children: [
-      { index: true, element: <RedirectPage/> },
-      {
-        path: 'welcome',
-        element: <Outlet />,
-        children: [
-          { index: true, element: <RedirectPage/> },
-          {
-            path: '1',
-            element: (
-              <div>1 <NavLink to="/welcome/2">下一页</NavLink></div>
-              // <SpringDemo />
-            )
-          },
-          {
-            path: '2',
-            element: (
-              <div>2 <NavLink to="/welcome/3">下一页</NavLink></div>
-            )
-          },
-          {
-            path: '3',
-            element: (
-              <div>3 <NavLink to="/welcome/4">下一页</NavLink></div>
-            )
-          },
-          {
-            path: '4',
-            element: (
-              <div>4 <NavLink to="/start">下一页</NavLink></div>
-            )
-          },
-        ]
-      },
-      {
-        path: 'start',
-        element: <div>start</div>
-      }
-    ]
-  }
-])
+
 root.render(
   <React.StrictMode>
     <RouterProvider router={routes}></RouterProvider>
---
+++ src/routes/router.tsx
@@ -0,0 +1,44 @@
+import { Outlet, createBrowserRouter } from 'react-router-dom'
+import { RedirectPage } from '../components/RedirectPage'
+import { Welcome1 } from '../pages/Welcome/components/Welcome1'
+import { Welcome2 } from '../pages/Welcome/components/Welcome2';
+import { Welcome3 } from '../pages/Welcome/components/Welcome3';
+import { Welcome4 } from '../pages/Welcome/components/Welcome4';
+
+export const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Outlet/>, // Outlet 类似vue中的 <RouterView />
+    errorElement: <RedirectPage/>,
+    children: [
+      { index: true, element: <RedirectPage/> },
+      {
+        path: 'welcome',
+        element: <Outlet />,
+        children: [
+          { index: true, element: <RedirectPage/> },
+          {
+            path: '1',
+            element: <Welcome1 />
+          },
+          {
+            path: '2',
+            element: <Welcome2 />
+          },
+          {
+            path: '3',
+            element: <Welcome3 />
+          },
+          {
+            path: '4',
+            element: <Welcome4 />
+          },
+        ]
+      },
+      {
+        path: 'start',
+        element: <div>start</div>
+      }
+    ]
+  }
+])
