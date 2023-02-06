f4f6cc11c5f9738666cff58a3fb8c719fd2a39d4
wlin00 <616294069@qq.com>
2023-02-06 18:56:46 +0800

    add: 添加button组件

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes/router';
 import 'virtual:uno.css'
 import './global.scss'
+import './assets/stylesheets/vars.scss'
+
 const div = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(div)
 
