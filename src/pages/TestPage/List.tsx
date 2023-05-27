14ef2b71c65b07b24804ee841bc1c42757f72ef7
wlin00 <616294069@qq.com>
2023-05-27 21:35:29 +0800

    add: 新增react减少重渲染示例

--- src/pages/TestPage/List.tsx
+++ src/pages/TestPage/List.tsx
@@ -1,37 +1,72 @@
-// 列表场景最小化，删除新增元素，最小化渲染demo
+// React Hook TodoList场景，使用useCallback、React.memo等手段最小化列表重渲染
 import * as React from 'react'
 import s from './List.module.scss'
-import { useState } from 'react';
-import { ListItem } from './ListItem';
+import { useState, useCallback } from 'react';
+import { MemoizedListItem } from './ListItem';
 
 interface IListProps {
-
 }
 
 export type Item = {
   name: string,
-  id: number
+  id: string,
+  isCompleted?: boolean
 }
 
 // 列表组件
 export const List: React.FC<IListProps> = () => {
   const [list, setList] = useState<Item[]>([
-    { name: 'Apple', id: 1 },
-    { name: 'Banana', id: 2 },
-    { name: 'Orange', id: 3 },
-    { name: 'Noodle', id: 4 },
-    { name: 'Watermelon', id: 5 },
+    { name: 'Apple', id: '1', isCompleted: false },
+    { name: 'Banana', id: '2', isCompleted: false },
+    { name: 'Orange', id: '3', isCompleted: false },
+    { name: 'Noodle', id: '4', isCompleted: false },
+    { name: 'Watermelon', id: '5', isCompleted: false },
   ])
-  const onDeleteFn = (id: number) => {
-    console.log('deleteId', id)
-    // setList((prev) => prev.filter((item) => item.id !== id))
+
+  // 删除列表元素
+  const handleDelete = useCallback((id: string) => {
+    setList((prev) => prev.filter((item) => item.id !== id))
+  }, [])
+
+  // 更新列表元素状态
+  const handleUpdate = useCallback((id: string) => {
+    // 由于 useState 的更新是异步的, 所以这里使用回调确保拿到更新后的list数组
+    setList((prev) => {
+      const listSlice = [...prev]
+      const updateIndex = listSlice.findIndex((item) => item.id === id)
+      const updateItem = listSlice.find((item) => item.id === id)!
+      listSlice.splice(updateIndex, 1, {
+        ...updateItem,
+        isCompleted: true
+      })
+      return listSlice
+    })
+ 
+  }, [])
+  
+  // 添加列表元素
+  const handleAdd = () => {
+    setList((prev) => [...prev, {
+      id: Math.random().toString(36).slice(2, 7),
+      name: 'AddItem'
+    }])
   }
-  return (
+
+  return (<>
     <div className={s.list}>
       {list.map((item: Item) => (
-        <ListItem item={item} key={item.id} onDelete={onDeleteFn} />
+        <MemoizedListItem 
+          key={item.id} 
+          item={item} 
+          onDelete={handleDelete} 
+          onUpdate={handleUpdate} 
+        />
       ))}
     </div>
+    <div className={s.addWrap}>
+      <span onClick={handleAdd}>add</span>
+    </div>
+  </>
   )
 }
 
--- src/pages/TestPage/ListItem.tsx
+++ src/pages/TestPage/ListItem.tsx
@@ -1,15 +1,34 @@
 import s from './List.module.scss'
 import { Item } from './List';
+import React from 'react';
 interface IListItemProps {
-  onDelete: (id: number) => any,
+  onDelete: (id: string) => any,
+  onUpdate: (id: string) => any,
   item: Item
 }
 // 列表元素组件
-export const ListItem: React.FC<IListItemProps> = ({ item, onDelete }) => {
+export const ListItem: React.FC<IListItemProps> = ({ item, onDelete, onUpdate }) => {
   return (
     <div className={s.listItem}>
-      <span>{item.name}</span>
-      <span className={s.button} onClick={onDelete(item.id)}>delete</span>
+      <span className={item.isCompleted ? s.isCompleted: ''}>{item.name}</span>
+      <span className={s.buttonWrap}>
+        <span className={[s.button, s.normalStyle].join(' ')} onClick={() => onUpdate(item.id)}>update</span>
+        <span className={s.button} onClick={() => onDelete(item.id)}>delete</span>
+      </span>
     </div>
   )
 }
+// 普通模式 - 删除/更新 任意一个ListItem会造成整个列表重渲染
+// export const MemoizedListItem = ListItem 
+
+// React.memo模式，默认写法，根据key比对，如果listItem的key相同则不进行重渲染
+// export const MemoizedListItem = React.memo(ListItem)
+
+// React.memo模式，使用参数2来自定义比对，当前后的props满足：id、和isCompleted字段都不变时可以不进行重渲染
+export const MemoizedListItem = React.memo(ListItem, 
+  (prevProps: any, nextProps: any) => 
+    prevProps.item.id === nextProps.item.id && 
+    prevProps.item.isCompleted === nextProps.item.isCompleted
+)
+
+
